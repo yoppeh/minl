71a107b86367d59b45ee74095050d920eee21157
Warren Mann <warren@nonvol.io>
2022-11-29 15:31:35 -0600

    Change package versions for lfs upgrade

--- install-stage1.sh
+++ install-stage1.sh
@@ -14,41 +14,41 @@ set -e
 
 # stage 1
 
-PASS=1 binutils_v='2.30' ./1-binutils.sh
-PASS=1 gcc_v='7.3.0' mpfr_v='4.0.1' gmp_v='6.1.2' mpc_v='1.1.0' ./1-gcc.sh
-linux_v='4.17.14' ./1-linux-headers.sh
-glibc_v='2.27' ./1-glibc.sh
-gcc_v='7.3.0' mpfr_v='4.0.1' gmp_v='6.1.2' mpc_v='1.1.0' ./1-libstdc++.sh
-PASS=2 binutils_v='2.30' ./1-binutils.sh
-PASS=2 gcc_v='7.3.0' mpfr_v='4.0.1' gmp_v='6.1.2' mpc_v='1.1.0' ./1-gcc.sh
-tcl_v='8.6.8' ./1-tcl.sh
+PASS=1 binutils_v='2.39' ./1-binutils.sh
+PASS=1 gcc_v='12.2.0' mpfr_v='4.1.0' gmp_v='6.2.1' mpc_v='1.2.1' ./1-gcc.sh
+linux_v='6.0.10' ./1-linux-headers.sh
+glibc_v='2.36' ./1-glibc.sh
+gcc_v='12.2.0' mpfr_v='4.1.0' gmp_v='6.2.1' mpc_v='1.2.1' ./1-libstdc++.sh
+PASS=2 binutils_v='2.39' ./1-binutils.sh
+PASS=2 gcc_v='12.2.0' mpfr_v='4.1.0' gmp_v='6.2.1' mpc_v='1.2.1' ./1-gcc.sh
+tcl_v='8.6.12' ./1-tcl.sh
 expect_v='5.45.4' ./1-expect.sh
-dejagnu_v='1.6.1' ./1-dejagnu.sh
-m4_v='1.4.18' ./1-m4.sh
-ncurses_v='6.1' ./1-ncurses.sh
-bash_v='4.4.18' ./1-bash.sh
-bison_v='3.0.4' ./1-bison.sh
-bzip2_v='1.0.6' ./1-bzip2.sh
-coreutils_v='8.29' ./1-coreutils.sh
-diffutils_v='3.6' ./1-diffutils.sh
-file_v='5.32' ./1-file.sh
-findutils_v='4.6.0' ./1-findutils.sh
-gawk_v='4.2.0' ./1-gawk.sh
-gettext_v='0.19.8.1' ./1-gettext.sh
-grep_v='3.1' ./1-grep.sh
-gzip_v='1.9' ./1-gzip.sh
-make_v='4.2.1' ./1-make.sh
+dejagnu_v='1.6.3' ./1-dejagnu.sh
+m4_v='1.4.19' ./1-m4.sh
+ncurses_v='6.3' ./1-ncurses.sh
+bash_v='5.1.16' ./1-bash.sh
+bison_v='3.8.2' ./1-bison.sh
+bzip2_v='1.0.8' ./1-bzip2.sh
+coreutils_v='9.1' ./1-coreutils.sh
+diffutils_v='3.8' ./1-diffutils.sh
+file_v='5.42' ./1-file.sh
+findutils_v='4.9.0' ./1-findutils.sh
+gawk_v='5.1.1' ./1-gawk.sh
+gettext_v='0.21' ./1-gettext.sh
+grep_v='3.7' ./1-grep.sh
+gzip_v='1.12' ./1-gzip.sh
+make_v='4.3' ./1-make.sh
 patch_v='2.7.6' ./1-patch.sh
-perl_v='5.26.1' ./1-perl.sh
-sed_v='4.4' ./1-sed.sh
-tar_v='1.30' ./1-tar.sh
-texinfo_v='6.5' ./1-texinfo.sh
-util_linux_v='2.31.1' ./1-util-linux.sh
-xz_v='5.2.3' ./1-xz.sh
+perl_v='5.36.0' ./1-perl.sh
+sed_v='4.8' ./1-sed.sh
+tar_v='1.34' ./1-tar.sh
+texinfo_v='6.8' ./1-texinfo.sh
+util_linux_v='2.38.1' ./1-util-linux.sh
+xz_v='5.2.6' ./1-xz.sh
 
-check_v='0.11.0' ./1-check.sh
-zlib_v='1.2.11' ./1-zlib.sh
-libcap_v='2.25' ./1-libcap.sh
+check_v='0.15.2' ./1-check.sh
+zlib_v='1.2.12' ./1-zlib.sh
+libcap_v='2.65' ./1-libcap.sh
 fakeroot_v='1.23' ./1-fakeroot.sh
 libarchive_v='3.3.2' ./1-libarchive.sh
 pacman_v='5.1.1' ./1-pacman.sh
--- install-stage3.sh
+++ install-stage3.sh
@@ -8,7 +8,7 @@ export PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
 set -e
 
-bootscripts_v=20150222 ./3-bootscripts.sh
+bootscripts_v=20220723 ./3-bootscripts.sh
 ./3-network.sh
 ./3-inittab.sh
 ./3-sysclock.sh
@@ -17,8 +17,8 @@ bootscripts_v=20150222 ./3-bootscripts.sh
 ./3-inputrc.sh
 ./3-shells.sh
 ./3-fstab.sh
-linux_v='4.9.2' ./3-kernel.sh
+linux_v='6.0.10' ./3-kernel.sh
 #./3-bootloader.sh
-blfs_bootscripts_v='20160902' ./3-blfs-bootscripts.sh
-dhcpcd_v='6.11.5' ./3-dhcpcd.sh
+blfs_bootscripts_v='20220722' ./3-blfs-bootscripts.sh
+dhcpcd_v='9.4.1' ./3-dhcpcd.sh
 ./3-finish.sh
