2189eff23c25bf8d84df3a39329dbe46c7f1914b
Warren Mann <warren@nonvol.io>
2022-11-29 20:50:14 -0600

    Update dhcpcd to BLFS 11.2

--- 3-dhcpcd.sh
+++ 3-dhcpcd.sh
@@ -2,13 +2,24 @@
 if [ -f $PROGRESS_DIR/2-dhcpcd ] ; then
 	exit 0
 fi
+
 echo "building dhcpcd..."
+
 set -e
+
+install -v -m700 -d /var/lib/dhcpcd
+groupadd -g 52 dhcpcd
+useradd -c 'dhcpcd PrivSep' -d /var/lib/dhcpcd -g dhcpcd -s /bin/false -u 52 dhcpcd
+chown -v dhcpcd:dhcpcd /var/lib/dhcpcd
+
 tar xf dhcpcd-${dhcpcd_v}.tar.xz
 cd dhcpcd-${dhcpcd_v}
-./configure --libexecdir=/lib/dhcpcd --dbdir=/var/lib/dhcpcd
+
+sed '/Deny everything else/i SECCOMP_ALLOW(__NR_getrandom),' -i src/privsep-linux.c
+./configure --prefix=/usr --sysconfdir=/etc --libexecdir=/usr/lib/dhcpcd --dbdir=/var/lib/dhcpcd --runstatdir=/run --privsepuser=dhcpcd
 make
 make install
+
 cat > /etc/sysconfig/ifconfig.eth0 << "EOF"
 ONBOOT="yes"
 IFACE="eth0"
@@ -16,6 +27,20 @@ SERVICE="dhcpcd"
 DHCP_START="-b -q"
 DHCP_STOP="-k"
 EOF
+
+cat > /etc/resolv.conf.head << "EOF"
+# OpenDNS servers
+nameserver 208.67.222.222
+nameserver 208.67.220.220
+ENOF
+
 cd ..
 rm -rf dhcpcd-${dhcpcd_v}
+
+tar xf blfs-bootscripts-${blfs_bootscripts_v}.tar.xz
+cd blfs-bootscripts-${blfs_bootscripts_v}
+make install-service-dhcpcd
+cd ..
+rm -rf blfs-bootscripts-${blfs_bootscripts_v}
+
 touch $PROGRESS_DIR/2-dhcpcd
